7b2cf6c21a69bb0bcd0169ddb39796d7daa994bd
Mahesh Kedari <kedari.mahesh@gmail.com>
2023-04-29 16:58:00 +0530

    Repeated text removed

--- WelcomeScreen.js
+++ WelcomeScreen.js
@@ -6,11 +6,6 @@ export default function WelcomeScreen() {
   return (
     <ScrollView indicatorStyle="white" style={styles.container} keyboardDismissMode='on-drag'>
       <Text style={styles.headerText}>Welcome to Little Lemon</Text>
-      <Text style={styles.regularText}>
-        Little Lemon is a charming neighborhood bistro that serves simple food
-        and classic cocktails in a lively but casual environment. We would love
-        to hear more about your experience with us!
-      </Text>
       <FeedbackForm />
     </ScrollView>
   );
